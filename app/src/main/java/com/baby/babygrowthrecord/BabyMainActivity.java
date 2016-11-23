8343e5b5bd4bcdd3d9cdca809160c64ec1500c4c
unknown <375705792@qq.com>
2016-11-23 19:12:34 +0800

    初始文档

---
+++ app/src/main/java/com/baby/babygrowthrecord/BabyMainActivity.java
@@ -0,0 +1,13 @@
+package com.baby.babygrowthrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BabyMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_main);
+    }
+}
