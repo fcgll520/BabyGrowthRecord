021cb667b4d7be6c63e1dec933578e4cd86f8baf
李宗谕 <375705792@qq.com>
2016-11-24 15:48:19 +0800

    圈子静态界面

--- app/src/main/java/com/baby/babygrowthrecord/BabyMainActivity.java
+++ app/src/main/java/com/baby/babygrowthrecord/BabyMainActivity.java
@@ -1,7 +1,10 @@
 package com.baby.babygrowthrecord;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.baby.babygrowthrecord.com.baby.babycircle.CircleMainActivity;
 
 public class BabyMainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class BabyMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baby_main);
+        Intent intent = new Intent(BabyMainActivity.this, com.baby.babygrowthrecord.com.baby.babycircle.CircleMainActivity.class);
     }
 }
---
+++ app/src/main/java/com/baby/babygrowthrecord/com/baby/babycircle/CircleMainActivity.java
@@ -0,0 +1,15 @@
+package com.baby.babygrowthrecord.com.baby.babycircle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baby.babygrowthrecord.R;
+
+public class CircleMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_circleitem);
+    }
+}
