e140bae30c1b3af53a5ff2c0839f6e6f7b2e245f
李宗谕 <375705792@qq.com>
2016-12-05 15:29:03 +0800

    沉浸栏颜色

--- app/src/main/java/com/baby/babygrowthrecord/Fragment/QuanziFragment.java
+++ app/src/main/java/com/baby/babygrowthrecord/Fragment/QuanziFragment.java
@@ -1,13 +1,18 @@
 package com.baby.babygrowthrecord.Fragment;
 
+import android.app.Activity;
 import android.app.ListFragment;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.baby.babygrowthrecord.Circle.FridListAdapter;
 import com.baby.babygrowthrecord.Circle.MessageModle;
@@ -30,6 +35,7 @@ public class  QuanziFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.activity_circle_main, container, false);
         imageLoader.init(ImageLoaderConfiguration.createDefault(getActivity()));
+        setColor(getActivity(), Color.parseColor("#63b68b") );
         new LoderDataTask().execute();
         return view;
     }
@@ -70,6 +76,34 @@ public class  QuanziFragment extends ListFragment {
                 + "{\"id\":113,\"avator\":\"http://img2.imgtn.bdimg.com/it/u=3301021828,1789093994&fm=11&gp=0.jpg\",\"name\":\"tracy\",\"content\":\"开心\",\"urls\":[\"http://img3.duitang.com/uploads/item/201505/22/20150522191353_QKRMx.thumb.700_0.jpeg\",\"http://img2.imgtn.bdimg.com/it/u=2725582282,1665252206&fm=21&gp=0.jpg\",\"http://img3.duitang.com/uploads/item/201607/05/20160705101741_NjMXK.jpeg\"]}]}";
 
         return json;
+    }
+    public static void setColor(Activity activity, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 设置状态栏透明
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 生成一个状态栏大小的矩形
+            View statusView = createStatusView(activity, color);
+            // 添加 statusView 到布局中
+            ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+            decorView.addView(statusView);
+            // 设置根布局的参数
+            ViewGroup rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+            rootView.setFitsSystemWindows(true);
+            rootView.setClipToPadding(true);
+        }
+    }
+    private static View createStatusView(Activity activity, int color) {
+        // 获得状态栏高度
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+
+        // 绘制一个和状态栏一样高的矩形
+        View statusView = new View(activity);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                statusBarHeight);
+        statusView.setLayoutParams(params);
+        statusView.setBackgroundColor(color);
+        return statusView;
     }
 
 }
