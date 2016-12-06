42cd259d74b80511d510420043b1a1dafd5c49ea
zhenyumin <2891488422@qq.com>
2016-12-06 16:22:14 +0800

    妈妈帮获取列表页内容

--- app/src/main/java/com/baby/babygrowthrecord/Fragment/HelpFragment.java
+++ app/src/main/java/com/baby/babygrowthrecord/Fragment/HelpFragment.java
@@ -4,16 +4,26 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.baby.babygrowthrecord.Mother.CardMessage;
 import com.baby.babygrowthrecord.Mother.GoogleCard;
 import com.baby.babygrowthrecord.Mother.GoogleCardAdapter;
 import com.baby.babygrowthrecord.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +35,18 @@ public class HelpFragment extends Fragment{
     private View view;
     private ListView mListView;
     private List<GoogleCard> mCards=new ArrayList<GoogleCard>();
+    private GoogleCardAdapter mAdapter;
+    final String url = "http://10.7.88.67:8080/essay/test";
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.activity_mother_main, container, false);
         getItems();
         //获取listview
         mListView=(ListView) view.findViewById(R.id.ListView);
         //配置适配器
-        GoogleCardAdapter mAdapter=new GoogleCardAdapter(getActivity(),mCards);
+        mAdapter = new GoogleCardAdapter(getActivity(),mCards);
         mListView.setAdapter(mAdapter);
         //给item设置监听
 
@@ -41,6 +54,8 @@ public class HelpFragment extends Fragment{
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent=new Intent(getActivity(),CardMessage.class);
+                intent.putExtra("essay_id",mCards.get(i).getmId());
+                Log.e("id:"+mCards.get(i).getmId(),"i:"+i);
                 startActivity(intent);
             }
         });
@@ -49,22 +64,26 @@ public class HelpFragment extends Fragment{
 
     private void getItems()
     {
+        //从服务器获取信息并解析
+        AsyncHttpClient client = new AsyncHttpClient();
 
-        //第一张卡片
-        GoogleCard mCard=new GoogleCard("纸尿裤的选择和使用",R.drawable.mother_item1);
-        mCards.add(mCard);
-
-        //第二张卡片
-        GoogleCard mCard1=new GoogleCard("就是他们让宝贝越来越笨！",R.drawable.mother_pic1);
-        mCards.add(mCard1);
-
-        //第三张卡片
-        GoogleCard mCard2=new GoogleCard("纸尿裤的选择和使用",R.drawable.mother_item1);
-        mCards.add(mCard2);
-
-        //第四张卡片
-        GoogleCard mCard3=new GoogleCard("纸尿裤的选择和使用",R.drawable.mother_item1);
-        mCards.add(mCard3);
+        client.get(getActivity(),url,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                JSONObject object;
+                for (int i=0;i<response.length();i++){
+                    try {
+                        object=response.getJSONObject(i);
+                        GoogleCard card=new GoogleCard(object.getInt("essay_id"),object.getString("essay_title"),object.getString("essay_photo"));
+                        mCards.add(card);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                mListView.setAdapter(mAdapter);
+            }
+        });
     }
 
 }
