c41561e2285452f6f8fb59552d991fc214e10e48
张斌 <925971874@qq.com>
2016-12-08 14:52:56 +0800

    Merge branch 'develop' of https://github.com/lizongyuyu/BabyGrowthRecord
    # Conflicts:
    #	README.md
    实现图片的点击事件，以及拍照,视频，添加本地图片

--- app/src/main/java/com/baby/babygrowthrecord/Fragment/PublishFragment.java
+++ app/src/main/java/com/baby/babygrowthrecord/Fragment/PublishFragment.java
@@ -1,14 +1,18 @@
 package com.baby.babygrowthrecord.Fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.baby.babygrowthrecord.MainActivity.BabyMainActivity;
 import com.baby.babygrowthrecord.R;
@@ -16,39 +20,51 @@ import com.baby.babygrowthrecord.R;
 /**
  * Created by asus on 2016/11/22.
  */
-public class PublishFragment extends Fragment{
-    private ImageView composer_btn;
-    private ImageButton composer_img_btn;
-    private View view;
-    @Nullable
+public class PublishFragment extends AppCompatActivity{
+    private ImageView composer_btn,composer_btn1,composer_btn2;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view =  inflater.inflate(R.layout.second, container, false);
-        return view;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        //1. 获取界面的控件
+        getViews();
+
+        //2. 注册事件监听器
+        setListener();
     }
-
-    @Override
-    public void onActivityCreated(final Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        composer_btn = (ImageView) view.findViewById(R.id.composer_btn);
-        composer_img_btn = (ImageButton) view.findViewById(R.id.composer_img_btn);
-
-        composer_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),PublishActivity.class);
-                startActivity(intent);
+    //获取界面的控件
+    private void getViews(){
+        composer_btn = (ImageView)findViewById(R.id.composer_btn);
+        composer_btn1 = (ImageView)findViewById(R.id.composer_btn1);
+        composer_btn2 = (ImageView)findViewById(R.id.composer_btn2);
+    }
+    //注册事件监听器
+    private void setListener(){
+        MyListener listener = new MyListener();
+        composer_btn.setOnClickListener(listener);
+        composer_btn1.setOnClickListener(listener);
+        composer_btn2.setOnClickListener(listener);
+    }
+    class MyListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.composer_btn:
+                    Intent intent = new Intent(PublishFragment.this,PublishActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.composer_btn1:
+                    Intent intent1 = new Intent(PublishFragment.this,PublishActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.composer_btn2:
+                    Intent intent2 = new Intent(PublishFragment.this,PublishActivity.class);
+                    startActivity(intent2);
+                    break;
             }
-        });
-
-        composer_img_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity(), BabyMainActivity.class);
-                startActivity(intent);
-            }
-        });
+        }
+    }
+    public void backOnClick(View view){
+        finish();
     }
 }
