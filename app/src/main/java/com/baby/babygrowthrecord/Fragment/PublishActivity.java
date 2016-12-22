2954166d9d96148061c491b9d29be0d97f0b1ad6
张斌 <925971874@qq.com>
2016-12-22 08:35:04 +0800

    Merge branch 'develop' of D:\github\BabyGrowthRecord with conflicts.

--- app/src/main/java/com/baby/babygrowthrecord/Fragment/PublishActivity.java
+++ app/src/main/java/com/baby/babygrowthrecord/Fragment/PublishActivity.java
@@ -38,6 +38,8 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baby.babygrowthrecord.Login.Register_Activity;
+import com.baby.babygrowthrecord.MainActivity.BabyMainActivity;
 import com.baby.babygrowthrecord.R;
 import com.baby.babygrowthrecord.activity.AlbumActivity;
 import com.baby.babygrowthrecord.util.Bimp;
@@ -45,6 +47,12 @@ import com.baby.babygrowthrecord.util.FileUtils;
 import com.baby.babygrowthrecord.util.ImageItem;
 import com.baby.babygrowthrecord.util.PublicWay;
 import com.baby.babygrowthrecord.util.Res;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @SuppressLint("HandlerLeak")
 public class PublishActivity extends AppCompatActivity {
@@ -54,23 +62,13 @@ public class PublishActivity extends AppCompatActivity {
     private Button button2;
     private EditText editText;
 
+    private GridView noScrollgridview;
     private GridAdapter adapter;
     private LinearLayout ll_popup;
-    private GridView noScrollgridview;
     public static Bitmap bimap ;
     private PopupWindow pop = null;
     private View parentView;
-    public TextView send_message ;
-    private EditText word_message ;
     private Uri photoUri ; //得到清晰的图片
-
-
-    private static final String IMAGE_UNSPECIFIED = "image/*";
-    private static final String TAG = "MyActivity";
-    private static final int ALBUM_REQUEST_CODE = 2;
-    private static final int CAMERA_REQUEST_CODE = 2;
-    private static final int CROP_REQUEST_CODE = 4;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,175 +81,65 @@ public class PublishActivity extends AppCompatActivity {
         parentView = getLayoutInflater().inflate(R.layout.activity_third, null);
         setContentView(parentView);
         Init();
-        //Sent_Message();
+
 
         button1 = (Button)findViewById(R.id.button1);
-        button2 = (Button)findViewById(R.id.button2);
-        editText = (EditText)findViewById(R.id.edit_text) ;
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*AlertDialog.Builder builder = new AlertDialog.Builder(PublishActivity.this);
-                builder.setTitle("你确定要取消发布并保存为草稿？");
-                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                 //这里添加点击确定后的逻辑
-                                Toast.makeText(PublishActivity.this, "已保存到草稿", Toast.LENGTH_SHORT).show();
-                                finish();
-                                //Intent intent = new Intent(PublishActivity.this, BabyMainActivity.class);
-                                //startActivity(intent);
-                            }
-                        });
-                builder.setNegativeButton("取消",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                finish();
-                            }
-                        });
-                builder.create().show();*/
                 finish();
             }
         });
+        editText = (EditText)findViewById(R.id.edit_text);
+        button2 = (Button)findViewById(R.id.button2);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*String context = editText.getText().toString();
-                String url = "http://169.254.254.2:8080/";
-                AsyncHttpClient client = new AsyncHttpClient();
-                client.get(PublishActivity.this, url + "circle/test?cir_content=" + context, new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        super.onSuccess(statusCode, headers, response);
-                        try {
-                            Utils.circlrId = response.getInt("cir_id");
-                            Utils.circleCntent = response.getString("cir_content");
-                            Utils.circlePhoto = response.getString("cir_photo");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                String text = editText.getText().toString();
+                if (text.equals("")){
+                    Toast.makeText(PublishActivity.this,"内容不能为空！！",Toast.LENGTH_SHORT).show();
+                }else{
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    client.get(PublishActivity.this,Utils.StrUrl+"circlr/register?cir_content="+text,new JsonHttpResponseHandler(){
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            super.onSuccess(statusCode, headers, response);
+                            if (response!=null){
+                                Toast.makeText(PublishActivity.this,"发布成功~",Toast.LENGTH_SHORT).show();
+                                try {
+                                    Utils.circlrId=response.getInt("cir_id");
+                                Utils.userName=response.getString("user_name");
+                                Utils.circleCntent=response.getString("cir_content");
+                                for(int i=0;i<PublicWay.activityList.size();i++){
+                                    if (null != PublicWay.activityList.get(i)) {
+                                        PublicWay.activityList.get(i);
+                                        Utils.circlePhoto = response.getString("cir_hphoto");
+                                    }
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                                Utils.flag = 1;
+                                Intent intent=new Intent(PublishActivity.this, BabyMainActivity.class);
+                                startActivity(intent);
+                            }else {
+                                Toast.makeText(PublishActivity.this,"由于网络问题发布失败，请确认后再试！",Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
+                    });
+                }
 
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                        super.onFailure(statusCode, headers, throwable, errorResponse);
-                        Toast.makeText(PublishActivity.this, "网络连接错误，请稍后再试！", Toast.LENGTH_SHORT).show();
-                        Log.e("REGISTER_ERROR", throwable.toString());
-                    }
-                });*/
-                finish();
             }
         });
     }
-        /*imageView_camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new android.app.AlertDialog.Builder(PublishActivity.this)
-                        .setPositiveButton(R.string.dialog_p_btn,
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Log.i(TAG, "相册");
-                                        Intent intent = new Intent(Intent.ACTION_PICK, null);
-                                        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_UNSPECIFIED);
-                                        startActivityForResult(intent, ALBUM_REQUEST_CODE);
-                                    }
-                                })
-                        .setNegativeButton(R.string.dialog_n_btn,
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Log.i(TAG, "相机");
-                                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "temp.jpg")));
-                                        startActivityForResult(intent, CAMERA_REQUEST_CODE);
-                                    }
-                                }).create().show();
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        switch (requestCode) {
-            case ALBUM_REQUEST_CODE:
-                Log.i(TAG, "相册，开始裁剪");
-                Log.i(TAG, "相册 [ " + data + " ]");
-                if (data == null) {
-                    return;
-                }
-                doCrop(data.getData());
-                break;
-            case CAMERA_REQUEST_CODE:
-                Log.i(TAG, "相机, 开始裁剪");
-                File picture = new File(Environment.getExternalStorageDirectory()
-                        + "/temp.jpg");
-                doCrop(Uri.fromFile(picture));
-                break;
-            case CROP_REQUEST_CODE:
-                Log.i(TAG, "相册裁剪成功");
-                Log.i(TAG, "裁剪以后 [ " + data + " ]");
-                if (data == null) {
-                    // TODO 如果之前以后有设置过显示之前设置的图片 否则显示默认的图片
-                    return;
-                }
-                Bundle extras = data.getExtras();
-                if (extras != null) {
-                    Bitmap photo = extras.getParcelable("data");
-                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                    photo.compress(Bitmap.CompressFormat.JPEG, 75, stream);// (0-100)压缩文件
-                    //此处可以把Bitmap保存到sd卡中，具体请看：http://www.cnblogs.com/linjiqin/archive/2011/12/28/2304940.html
-                    imageView_camera.setImageBitmap(photo); //把图片显示在ImageView控件上
-                }
-                break;
-            default:
-                break;
-        }
-    }
-    private void doCrop(Uri uri) {
-        Intent intent = new Intent("com.android.camera.action.CROP");//调用Android系统自带的一个图片剪裁页面,
-        intent.setDataAndType(uri, IMAGE_UNSPECIFIED);
-        intent.putExtra("crop", "true");//进行修剪
-        // aspectX aspectY 是宽高的比例
-        intent.putExtra("aspectX", 1);
-        intent.putExtra("aspectY", 1);
-        // outputX outputY 是裁剪图片宽高
-        intent.putExtra("outputX", 200);
-        intent.putExtra("outputY", 200);
-        intent.putExtra("return-data", true);
-        startActivityForResult(intent, CROP_REQUEST_CODE);
-    }*/
     /**
      * 判断sdcard卡是否可用
      *
      * @return 布尔类型 true 可用 false 不可用
      */
-    /*private boolean isSDCardCanUser() {
+    private boolean isSDCardCanUser() {
         return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
-    }*/
-
-
-    /*public void Sent_Message(){
-        button2 = (Button) findViewById(R.id.button2);
-        editText = (EditText) findViewById(R.id.edit_text);
-
-        send_message.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if(word_message.getText().toString() == null || word_message.getText().toString().length()==0){
-                    Toast.makeText(PublishActivity.this, "内容不能为空", Toast.LENGTH_LONG).show();
-                }else{
-                    new Thread(networkTask).start();
-                    //System.out.println(adapter.getItem(1).toString());
-                    Toast.makeText(PublishActivity.this, adapter.getCount()+"信息是："+(word_message.getText().toString() == null) +word_message.getText().toString().length()+word_message.getText().toString(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-    }*/
-
-
+    }
 
     public void Init(){
         pop = new PopupWindow(PublishActivity.this);
@@ -292,26 +180,11 @@ public class PublishActivity extends AppCompatActivity {
         });
         bt2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               /* Intent intent = new Intent(Intent.ACTION_PICK, null);
-                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_UNSPECIFIED);
-                startActivityForResult(intent, ALBUM_REQUEST_CODE);*/
                 Intent intent = new Intent(PublishActivity.this, AlbumActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.activity_translate_in, R.anim.activity_translate_out);
                 pop.dismiss();
                 ll_popup.clearAnimation();
-
-               /* new android.app.AlertDialog.Builder(PublishActivity.this)
-                        .setPositiveButton(R.string.dialog_p_btn,
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Intent intent = new Intent(Intent.ACTION_PICK, null);
-                                        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_UNSPECIFIED);
-                                        startActivityForResult(intent, ALBUM_REQUEST_CODE);
-                                    }
-                                });
-                */
             }
         });
         bt3.setOnClickListener(new View.OnClickListener() {
